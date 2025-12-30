f22351fd02e139249d109f34636215b2fc2af940
saniyakawle4-lab <saniyakawle4@gmail.com>
2025-12-30 11:09:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+fruits.forEach((fruit, index) => {
+  console.log(index + 1 + ". " + fruit);
+});
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+fruits.forEach((fruit, index) => {
+  console.log(index + 1 + ". " + fruit);
+});
